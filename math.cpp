fbc7617667529ed4b77b7d05ab299a95951f9885
Stefano Ariotta <sqrtstefano@gmail.com>
2021-10-22 17:11:51 +0200

    introduced new 1D rngs for better performace

--- math.cpp
+++ math.cpp
@@ -1,49 +1,159 @@
 #include "math.h"
 #include "extern/glm/glm/gtx/norm.hpp"
 
-#include <random>
 #include <thread>
 #include <time.h>
 
-#define STATIC_RANDOM 1
-#ifdef STATIC_RANDOM
+// if not defined, random_float_pair() generates unstratified pairs
+//#define PMJ02_RANDOM_PAIRS 1
+#ifdef PMJ02_RANDOM_PAIRS
 #include "rng.h"
 #include <unordered_map>
 #endif
 
-size_t random_size_t(size_t min, size_t max)
+// macros determining which prng is used
+// always comment all but one
+
+//#define XORSHIFT_RANDOM 1
+#ifdef XORSHIFT_RANDOM
+#include <bitset>
+#endif
+
+#define XOROSHIRO128PLUS_RANDOM 1
+#ifdef XOROSHIRO128PLUS_RANDOM
+#include <bitset>
+#endif
+
+//#define STD_RNG 1
+#ifdef STD_RNG
+#include <random>
+#endif
+
+static inline uint64_t xoroshiro_rotl(const uint64_t x, int k)
 {
+  return (x << k) | (x >> (64 - k));
+}
+
+uint32_t random_uint32_t(uint16_t x, uint16_t y, uint16_t z)
+{
+  #ifdef XORSHIFT_RANDOM
+  // xorshift
+  // seed depending on x, y, and z that is nontrivial even if they're all 0
+  static thread_local uint32_t hash{
+    ((uint32_t(x) ^ (uint32_t(z) << 8)) | ((uint32_t(y) ^ (uint32_t(z) >> 8)) << 16)) | 0xE6D5258A
+  };
+  uint32_t n{hash};
+  n ^= n >> 13;
+  n ^= n << 17;
+  n ^= n >> 5;
+  hash = n;
+
+  return n;
+  #elif defined(XOROSHIRO128PLUS_RANDOM)
+  //xoroshiro128+
+
+  // seeds depending on x, y, and z that are nontrivial even if they're all 0
+  static thread_local std::array<uint64_t,2> s{
+     uint64_t(x) |
+     (uint64_t(y) << 16) |
+     (uint64_t(z) << 32) |
+     (uint64_t(x ^ y) << 48) |
+     uint64_t(0xBD6CB273039BC9C0),
+
+     uint64_t(y^z) |
+     (uint64_t(x) << 16) |
+     (uint64_t(y) << 32) |
+     (uint64_t(z) << 48) |
+     uint64_t(0xC7B38377DB5B4910)
+  };
+
+  const uint64_t s0{s[0]};
+  uint64_t s1{s[1]};
+  const uint32_t res{uint32_t((s0 + s1) >> 32)};
+
+  // update seed
+  s1 ^= s0;
+  s[0] = xoroshiro_rotl(s0, 24) ^ s1 ^ (s1 << 16); // a, b
+  s[1] = xoroshiro_rotl(s1, 37); // c
+
+  return res;
+  #elif defined(STD_RNG)
   static thread_local std::mt19937_64 generator(std::clock()
     + std::hash<std::thread::id>()(std::this_thread::get_id()));
-  std::uniform_int_distribution<size_t> distribution(min, max);
+  std::uniform_int_distribution<uint32_t> distribution(0);
   return distribution(generator);
+  #endif
+}
+
+uint32_t random_uint32_t(uint16_t x, uint16_t y, uint16_t z, uint32_t range)
+{
+  // unbiased fast ranged rng, due to D. Lemire and M.E. O'Neill
+  // (see https://www.pcg-random.org/posts/bounded-rands.html)
+  uint32_t n{random_uint32_t(x,y,z)};
+  uint64_t m{uint64_t(n) * uint64_t(range)};
+  uint32_t l{uint32_t(m)};
+  if (l < range)
+  {
+    uint32_t t{-range};
+    if (t >= range)
+    {
+        t -= range;
+        if (t >= range)
+            t %= range;
+    }
+    while (l < t)
+    {
+        n = random_uint32_t(x,y,z);
+        m = uint64_t(n) * uint64_t(range);
+        l = uint32_t(m);
+    }
+  }
+  return m >> 32;
 }
 
 float random_float(uint16_t x, uint16_t y, uint16_t z)
 {
+  #if defined(XORSHIFT_RANDOM) || defined(XOROSHIRO128PLUS_RANDOM)
+  uint32_t n{random_uint32_t(x,y,z)};
+  // convert the uint32 into a float:
+  // mask out the 23 bits of the mantissa, and fix the first 9 bits to make it positive and
+  // concentrated in [1,2) (set the exponent to 127)
+  std::bitset<32> float_bits{(n & 0x007FFFFF) | 0x3F800000};
+  float res;
+  std::memcpy(&res,&float_bits,32);
+
+  // shift the result to make it concentrated in [0,1)
+  return res - 1.0f;
+  #elif defined(STD_RNG)
   static thread_local std::mt19937_64 generator(std::clock()
     + std::hash<std::thread::id>()(std::this_thread::get_id()));
   static std::uniform_real_distribution<float> distribution(0.0f, 1.0f);
   return distribution(generator);
+  #endif
 }
 
 std::array<float,2> random_float_pair(uint16_t x, uint16_t y, uint16_t z)
 {
-  #ifdef STATIC_RANDOM
+  #ifdef PMJ02_RANDOM_PAIRS
   static thread_local std::unordered_map<uint64_t,size_t> indices;
-  static thread_local size_t shuffle_filter{random_size_t(0,SIZE_RNG_SAMPLES-1)};
+  static thread_local const uint16_t
+    shuffle_filter{uint16_t(random_uint32_t(x,y,z,SIZE_RNG_SAMPLES-1) >> 16)};
+  // scramble_filter, to scramble the (mantissa bits of) the sample
+  // see Fredel--Keller, Fast Generation of Randomized Low-Discrepancy Point Sets
+  // and Kollig--Keller, Efficient Multidimensional Sampling
+  static thread_local std::unordered_map<uint64_t,uint32_t> scramble_filters;
   uint64_t hash{uint64_t(x) | (uint64_t(y) << 16) | (uint64_t(z) << 32)};
 
   if (indices.find(hash) == indices.end())
+  {
     indices[hash] = 0;
+    scramble_filters[hash] = random_uint32_t(x,y,z) >> 9;
+  }
   else
     indices[hash] += 1;
 
   uint16_t sample{uint16_t(hash % N_RNG_SAMPLES)};
-  /* the following is too slow;
-  // for now, if the index exceeds SIZE_RNG_SAMPLES increments, it cycles back
-  // it should be fine like this
-  // TODO test for how many times a wraparound actually happens
+
   uint16_t offset{uint16_t(indices[hash] / SIZE_RNG_SAMPLES)};
 
   if (offset > 0)
@@ -51,9 +161,17 @@ std::array<float,2> random_float_pair(uint16_t x, uint16_t y, uint16_t z)
     sample += offset;
     sample %= N_RNG_SAMPLES;
   }
-  */
 
-  return (*samples_2d[sample])[(indices[hash] % SIZE_RNG_SAMPLES) ^ uint16_t(shuffle_filter)];
+  float res0{(*samples_2d[sample])[(indices[hash] % SIZE_RNG_SAMPLES) ^ shuffle_filter][0]};
+  float res1{(*samples_2d[sample])[(indices[hash] % SIZE_RNG_SAMPLES) ^ shuffle_filter][1]};
+
+  uint32_t temp0{reinterpret_cast<uint32_t&>(res0) ^ scramble_filters[hash]};
+  uint32_t temp1{reinterpret_cast<uint32_t&>(res1) ^ scramble_filters[hash]};
+
+  std::memcpy(&res0,&temp0,32);
+  std::memcpy(&res1,&temp1,32);
+
+  return std::array<float,2>{res0,res1};
   #else
   return std::array<float,2>{random_float(x,y,z), random_float(x,y,z)};
   #endif
@@ -61,10 +179,14 @@ std::array<float,2> random_float_pair(uint16_t x, uint16_t y, uint16_t z)
 
 float random_float(float min, float max, uint16_t x, uint16_t y, uint16_t z)
 {
+  #if defined(XORSHIFT_RANDOM) || defined(XOROSHIRO128PLUS_RANDOM)
+  return min + (max - min) * random_float(x,y,z);
+  #elif defined(STD_RNG)
   static thread_local std::mt19937_64 generator(std::clock()
     + std::hash<std::thread::id>()(std::this_thread::get_id()));
   std::uniform_real_distribution<float> distribution(min, max);
   return distribution(generator);
+  #endif
 }
 
 float fast_inverse_sqrt(float x) // https://en.wikipedia.org/wiki/Fast_inverse_square_root
