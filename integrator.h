4a4a3023aa39e59d5db29c7503844e7984605d90
Stefano Ariotta <sqrtstefano@gmail.com>
2021-11-24 16:51:41 +0100

    corrected and improved integrator implementation

--- integrator.h
+++ integrator.h
@@ -4,14 +4,23 @@
 #include "bvh.h"
 #include "rng.h"
 
+class brdf;
 class integrator
 {
   public:
     explicit integrator(uint64_t seed)
     : sampler{seed} {}
 
-    color integrate_path( const ray& r
-                        , const bvh_tree& world);
+    color integrate_path( ray& r
+                        , const bvh_tree& world) const;
   private:
+    color sample_light( const point& x
+                      , const normed_vec3& gnormal
+                      , const normed_vec3& snormal
+                      , const normed_vec3& incoming_dir
+                      , const hit_record& record
+                      , const bvh_tree& world
+                      , const brdf& b) const;
+
     sampler_1d sampler;
 };
